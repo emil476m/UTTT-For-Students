b7083cd99316d33fb98eed7be1bd112544b95d90
emil476m <emilwiltmann.dk@gmail.com>
2023-03-07 12:14:20 +0100

    fixed the bot

--- src/dk/easv/bll/bot/TestBOTEmil.java
+++ src/dk/easv/bll/bot/TestBOTEmil.java
@@ -14,33 +14,27 @@ public class TestBOTEmil implements IBot{
 
     @Override
     public IMove doMove(IGameState state) {
+        //state.getField().getMacroboard().
         List<IMove> moves = state.getField().getAvailableMoves();
         IMove moveToTie = testNearWin(state);
+        //System.out.println("The not random Move " + moveToTie);
         if (moveToTie != null) {
+            //System.out.println("Not Random");
             return moveToTie;
         }
         else {
             if (moves.size() > 0) {
+                //System.out.println("Random Move");
                 return moves.get(rand.nextInt(moves.size())); /* get random move from available moves */
             }
         }
         return null;
     }
 
-    private List<IMove> getGutMove(IGameState state){
-        List<IMove> moves =  state.getField().getAvailableMoves();
-        List<IMove> counterMove;
-        for (IMove move: moves){
-            if (move.getX() == 1 && move.getY() == 1){
-
-            }
-
-        }
-        return null;
-    }
 
     private IMove testNearWin(IGameState state){
         List<IMove> moves =  state.getField().getAvailableMoves();
+        //System.out.println("All available Moves: " + moves.toString());
 
         int winV0 = 0;
         int winV1 = 0;
@@ -50,6 +44,9 @@ public class TestBOTEmil implements IBot{
         int winH2 = 0;
         int winS0 = 0;
         int winS1 = 0;
+
+        int xMod = 0;
+        int yMod = 0;
 
         IMove move00 = null;
         IMove move01 = null;
@@ -62,53 +59,84 @@ public class TestBOTEmil implements IBot{
         IMove move22 = null;
 
         for (IMove move: moves){
-            int count1 = 0;
-            if (move.getX() == 0 && move.getY() == 0){
+            if (move.getX() <=2){
+                if(move.getY() <= 5 && move.getY()>2){
+                    yMod = 3;
+                }
+                else {
+                    yMod = 6;
+                }
+            }
+            else if (move.getX() <=5 && move.getX() >2){
+                xMod = 3;
+                if(move.getY() <= 5 && move.getY()>2){
+                    yMod = 3;
+                }
+                else {
+                    yMod = 6;
+                }
+            }
+            else{
+                xMod = 6;
+                if(move.getY() <= 5 && move.getY()>2){
+                    yMod = 3;
+                }
+                else {
+                    yMod = 6;
+                }
+            }
+        }
+
+
+
+
+        for (IMove move: moves){
+            if (move.getX() == 0+xMod && move.getY() == 0+yMod){
                 winV0++;
                 winH0++;
                 winS0++;
                 move00 = move;
             }
-            if (move.getX() == 0 && move.getY() == 1){
+            else if (move.getX() == 0+xMod  && move.getY() == 1+yMod){
                 winV0++;
                 winH1++;
                 move01 = move;
             }
-            if (move.getX() == 0 && move.getY() == 2){
+            else if (move.getX() == 0+xMod  && move.getY() == 2+yMod){
                 winV0++;
                 winH2++;
                 winS1++;
                 move02 = move;
             }
-            if (move.getX() == 1 && move.getY() == 0){
+            else if (move.getX() == 1+xMod  && move.getY() == 0+yMod){
                 winV1++;
                 winH0++;
                 move10 = move;
             }
-            if (move.getX() == 1 && move.getY() == 1){
+            else if (move.getX() == 1+xMod  && move.getY() == 1+yMod){
                 winV1++;
                 winH1++;
                 winS0++;
                 winS1++;
                 move11 = move;
             }
-            if (move.getX() == 1 && move.getY() == 2){
+            else if (move.getX() == 1+xMod  && move.getY() == 2+yMod){
                 winV1++;
                 winH2++;
                 move12 = move;
             }
-            if (move.getX() == 2 && move.getY() == 0){
+            else if (move.getX() == 2+xMod  && move.getY() == 0+yMod){
                 winV2++;
                 winH0++;
                 winS1++;
                 move20 = move;
             }
-            if (move.getX() == 2 && move.getY() == 1){
+            else if (move.getX() == 2+xMod  && move.getY() == 1+yMod){
                 winV2++;
                 winH1++;
                 move21 = move;
             }
-            if (move.getX() == 2 && move.getY() == 2){
+            else if (move.getX() == 2+xMod  && move.getY() == 2+yMod){
                 winV2++;
                 winH2++;
                 winS0++;
@@ -116,7 +144,7 @@ public class TestBOTEmil implements IBot{
             }
         }
 
-        if(winV0 <=1){
+        if(winV0 == 1){
             if (move00 != null)
                 return  move00;
             if (move01 != null)
@@ -124,7 +152,7 @@ public class TestBOTEmil implements IBot{
             if (move02 != null)
                 return  move02;
         }
-        if(winV1 <=1){
+        if(winV1 == 1){
             if (move10 != null)
                 return  move10;
             if (move11 != null)
@@ -132,7 +160,7 @@ public class TestBOTEmil implements IBot{
             if (move12 != null)
                 return  move12;
         }
-        if(winV2 <=1){
+        if(winV2 == 1){
             if (move20 != null)
                 return  move20;
             if (move21 != null)
@@ -140,7 +168,7 @@ public class TestBOTEmil implements IBot{
             if (move22 != null)
                 return  move22;
         }
-        if(winH0 <=1){
+        if(winH0 == 1){
             if (move00 != null)
                 return  move00;
             if (move10 != null)
@@ -148,7 +176,7 @@ public class TestBOTEmil implements IBot{
             if (move20 != null)
                 return  move20;
         }
-        if(winH1 <=1){
+        if(winH1 == 1){
             if (move01 != null)
                 return  move01;
             if (move11 != null)
@@ -156,7 +184,7 @@ public class TestBOTEmil implements IBot{
             if (move21 != null)
                 return  move21;
         }
-        if(winH2 <=1){
+        if(winH2 == 1){
             if (move02 != null)
                 return  move02;
             if (move12 != null)
@@ -164,7 +192,7 @@ public class TestBOTEmil implements IBot{
             if (move22 != null)
                 return  move22;
         }
-        if(winS0 <=1){
+        if(winS0 == 1){
             if (move00 != null)
                 return  move00;
             if (move11 != null)
@@ -172,7 +200,7 @@ public class TestBOTEmil implements IBot{
             if (move22 != null)
                 return  move22;
         }
-        if(winS1 <=1){
+        if(winS1 == 1){
             if (move20 != null)
                 return  move20;
             if (move11 != null)
@@ -180,6 +208,10 @@ public class TestBOTEmil implements IBot{
             if (move02 != null)
                 return  move02;
         }
+
+
+        //System.out.println("winV0: " + winV0 + " winV1 " +winV1 + " winV2 " + winV2 + " winH0 " + winH0 + " winH1 " +
+                //winH1 + " winH2 " + winH2 + " winS0 " + winS0 + " winS1 "+ winS1);
         return null;
     }
 
